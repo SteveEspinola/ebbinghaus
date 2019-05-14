059044ab4a40d7f2c73b5b3722685b93cf136170
Steve Espinola <stephen.espinola@gmail.com>
2019-05-13 19:32:36 -0700

    second commit

---
+++ ebbinghaus.py
@@ -0,0 +1,26 @@
+answer = ''
+while True == True:
+    answer = input('What is the capital of Arkansas? : ')
+    if answer == 'quit':
+        break
+    elif answer ==  'Little Rock':
+        print('Correct!\n')
+    else:
+        print('Sorry. The answer is Little Rock\n')
+# TODO
+# Change color on question, anwer and response, right and wrong
+# Add read file for questions and answers
+# Give option to choose a topic
+# Add spaced learning
+# Suggest topics based on spaced learning
+# Allow for timed answers and overall time of quiz
+# Allow for rverse questions 
+# Add to github
+# Add reamde
+# Add to git
+# Add multiple chiosce
+# Create tests from wikipedia or other sources
+# Fill in the blank
+
+
+
